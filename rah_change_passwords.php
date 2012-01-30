e80e57525b92c1338ee5acb0f767671429a7fbd4
Jukka Svahn <web@rahinaa.biz>
2012-01-30 03:49:07 +0200

    Adding v0.3 from SVN.

--- rah_change_passwords.php
+++ rah_change_passwords.php
@@ -1,17 +1,22 @@
 <?php	##################
 	#
 	#	rah_change_passwords-plugin for Textpattern
-	#	version 0.2
+	#	version 0.3
 	#	by Jukka Svahn
 	#	http://rahforum.biz
 	#
 	###################
 
-	if (@txpinterface == 'admin') {
+	if(@txpinterface == 'admin') {
 		add_privs('rah_change_passwords','1');
 		register_tab('extensions','rah_change_passwords','Change passwords');
 		register_callback('rah_change_passwords','rah_change_passwords');
+		register_callback('rah_change_passwords_head','admin_side','head_end');
 	}
+
+/**
+	Delivers the panes
+*/
 
 	function rah_change_passwords() {
 		global $step;
@@ -22,6 +27,36 @@
 			rah_change_passwords_edit();
 	}
 
+/**
+	Adds styles to the <head>
+*/
+
+	function rah_change_passwords_head() {
+		global $event;
+
+		if($event != 'rah_change_passwords')
+			return;
+
+		echo <<<EOF
+			<style type="text/css">
+				#rah_change_passwords_container {
+					width: 950px;
+					margin: 0 auto;
+				}
+				#rah_change_passwords_container select {
+					width: 450px;
+				}
+				#rah_change_passwords_container input.edit {
+					width: 940px;
+				}
+			</style>
+EOF;
+	}
+
+/**
+	The main pane
+*/
+
 	function rah_change_passwords_edit($message='') {
 		
 		global $event;
@@ -29,15 +64,16 @@
 		pagetop('Change passwords',$message);
 
 		echo 
-			'	<form method="post" action="index.php" style="width:950px;margin:0 auto;">'.n.
+			'	<form method="post" action="index.php" id="rah_change_passwords_container">'.n.
 			'		<h1><strong>rah_change_passwords</strong> | Reset user passwords</h1>'.n.
 			'		<p>&#187; <a href="?event=plugin&amp;step=plugin_help&amp;name=rah_change_passwords">Documentation</a></p>'.n.
 			
 			
 			'		<p>'.n.
-			'			<label for="rah_user_id">User</label><br />'.n.
-			'			<select style="width:450px;" name="user_id" id="rah_user_id">'.n.
-			'				<option value="">Select a user to modify...</option>'.n;
+			'			<label>'.n.
+			'				User<br />'.n.
+			'				<select name="user_id">'.n.
+			'					<option value="">Select a user to modify...</option>'.n;
 		
 		$rs = 
 			safe_rows(
@@ -47,33 +83,42 @@
 			);
 		
 		foreach($rs as $a) 
-			echo '				<option value="'.htmlspecialchars($a['user_id']).'">'.htmlspecialchars($a['name']).'</option>'.n;
+			echo '					<option value="'.htmlspecialchars($a['user_id']).'">'.htmlspecialchars($a['name']).'</option>'.n;
 		
 		echo 
-			'			</select>'.n.
+			'				</select>'.n.
+			'			</label>'.n.
 			'		</p>'.n.
 			'		<p>'.n.
-			'			<label for="rah_password">New password</label><br />'.n.
-			'			<input class="edit" type="password" name="pass" id="rah_password" value="" style="width:940px;" />'.n.
+			'			<label>'.n.
+			'				New password<br />'.n.
+			'				<input class="edit" type="password" name="pass" value="" />'.n.
+			'			</label>'.n.
 			'		</p>'.n.
 			'		<p>'.n.
-			'			<label for="rah_confirm">Confirm new password</label><br />'.n.
-			'			<input class="edit" type="password" name="confirm" id="rah_confirm" value="" style="width:940px;" />'.n.
+			'			<label>'.n.
+			'				Confirm new password<br />'.n.
+			'				<input class="edit" type="password" name="confirm" value="" />'.n.
+			'			</label>'.n.
 			'		</p>'.n.
-			
 			'		<p>'.n.
-			'			<label for="rah_email_password">Email the password to the user?</label><br />'.n.
-			'			<select style="width:450px;" name="email_password" id="rah_email_password">'.n.
-			'				<option value="yes">Yes</option>'.n.
-			'				<option value="no">No</option>'.n.
-			'			</select>'.n.
+			'			Email the password to the user?<br />'.n.
+			'			<label>'.n.
+			'				<input type="radio" name="email_password" value="yes" checked="checked" /> Yes'.n.
+			'			</label>'.n.
+			'			<label>'.n.
+			'				<input type="radio" name="email_password" value="no" /> No'.n.
+			'			</label>'.n.
 			'		</p>'.n.
-			
 			'		<input type="submit" value="Change the password" class="publish" />'.n.
 			'		<input type="hidden" name="event" value="'.$event.'" />'.n.
 			'		<input type="hidden" name="step" value="rah_change_passwords_save" />'.n.
 			'	</form>'.n;
 	}
+
+/**
+	Saves the changes
+*/
 
 	function rah_change_passwords_save() {
 		extract(gpsa(array(
@@ -83,8 +128,8 @@
 			'email_password'
 		)));
 		
-		if(empty($pass) or empty($confirm) or empty($user_id)) {
-			rah_change_passwords_edit('Password or user can\'t be empty.');
+		if(empty($pass) || empty($confirm) || empty($user_id)) {
+			rah_change_passwords_edit('Password or user fields are required.');
 			return;
 		}
 		
@@ -117,7 +162,7 @@
 			return;
 		}
 		
-		if($email_password == 'no') {
+		if($email_password != 'yes') {
 			rah_change_passwords_edit('Password changed.');
 			return;
 		}
