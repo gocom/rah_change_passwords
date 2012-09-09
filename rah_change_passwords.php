477f5bafeb1b035bf1714e64a82f1a1c912a746c
Jukka Svahn <web@rahinaa.biz>
2012-09-10 02:40:51 +0300

    Updated date.

--- rah_change_passwords.php
+++ rah_change_passwords.php
@@ -10,7 +10,7 @@
  *
  * Requires Textpattern v4.5.0 or newer.
  *
- * Copyright (C) 2011 Jukka Svahn <http://rahforum.biz>
+ * Copyright (C) 2012 Jukka Svahn <http://rahforum.biz>
  * Licensed under GNU Genral Public License version 2
  * http://www.gnu.org/licenses/gpl-2.0.html
  */
